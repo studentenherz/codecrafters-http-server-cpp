0d523f5c8de32003b062aacf3681874af20359ca
Michel Romero <studentenherz@gmail.com>
2024-03-09 06:08:48 -0300

    Add methos specific routers

--- src/dispatcher.hpp
+++ src/dispatcher.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <functional>
 #include <vector>
 #include <regex>
@@ -8,18 +9,26 @@
 #include "request.hpp"
 #include "response.hpp"
 
-using handler_t = std::function<HttpResponse(const HttpRequest&, std::vector<std::string>)>;
+using handler_t = std::function<HttpResponse(HttpRequest&, std::vector<std::string>)>;
 
 class Dispatcher {
-	std::vector<std::pair<std::string, handler_t>> _routes;
+	std::array<std::vector<std::pair<std::string, handler_t>>, HttpMethod::HttpMethodEnumSize> _routes;
 public:
-	Dispatcher& route(std::string path, handler_t handler){
-		_routes.push_back({path, handler});
+	Dispatcher& route(std::string path, handler_t handler, HttpMethod method = HttpMethod::GET){
+		_routes[method].push_back({path, handler});
 		return *this;
 	}
 
-	void dispatch(const HttpRequest& req, int socket_fd){
-		for (auto [path, handler]: _routes){
+	Dispatcher& get(std::string path, handler_t handler){
+		return route(path, handler);
+	}
+
+	Dispatcher& post(std::string path, handler_t handler){
+		return route(path, handler, HttpMethod::POST);
+	}
+
+	void dispatch(HttpRequest& req, int socket_fd){
+		for (auto [path, handler]: _routes[req.method]){
 			std::regex re(path);
 			std::smatch sm;
 			if (std::regex_match(req.path, sm, re)){
